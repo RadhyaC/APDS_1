001fb851ec76c41524ead88db7b9d987dfc3d0d6
RadhyaC <ST10069610@vcconnect.edu.za>
2024-08-09 20:17:24 +0200

    made changes

---
+++ Backend/server.mjs
@@ -0,0 +1,12 @@
+import http from "http";
+
+//set the port
+const PORT = 3000;
+
+// start the server
+const server = http.createServer((req,res)=>{
+res.end('I am crying less because my server is working')
+})
+
+server. listen(PORT)
+
