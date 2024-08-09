d88290829ee5edbe78f989602ac66e767a0b834f
RadhyaC <ST10069610@vcconnect.edu.za>
2024-08-09 22:16:19 +0200

    changed the port

--- Backend/server.mjs
+++ Backend/server.mjs
@@ -2,10 +2,14 @@ import express from "express";
 const PORT = 3000;
 const app = express();
 
-app. use(express.json());
+app.use(express.json());
 
-app.get('/',(req, res)=>{
-    res. send('I am finally figuring this out, no more crying')
+app.get('/', (req, res)=> {
+    res.send('I am finally figuring this out, no more crying')
+})
+
+app.get('/test',(req, res)=>{
+    res. send('Testing the /test endpoint')
 })
 
 // start the Express server
