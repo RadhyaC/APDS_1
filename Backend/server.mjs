8bc97c3adeb6729f6cec27e3566d96e1b00ef3d0
RadhyaC <ST10069610@vcconnect.edu.za>
2024-08-12 10:20:29 +0200

    did more stuff

--- Backend/server.mjs
+++ Backend/server.mjs
@@ -1,36 +1,11 @@
-import express from "express";
+import https from "https";
+import fs from "fs";
+//import app from "./app.mjs";
+
 const PORT = 3000;
-const app = express();
-const urlprefix = '/api';
 
-app.use(express.json());
-
-app.get(urlprefix+'/', (req, res)=> {
-    res.send('I am finally figuring this out, no more crying')
+const server = https.createServer({
+    key: fs.readFileSync('Keys/privatekey.pem'),
+    cert: fs.readFileSync('Keys/certificate.pem')
 })
-
-app.get(urlprefix+'/orders', (req, res)=>{
-    const orders = [
-        {
-            id: "1",
-            name: "Orange"
-        },
-        {
-            id: "2",
-            name: "Apple"
-        },
-        {
-            id: "3",
-            name: "Pear"
-        }
-    ]
-    res.json(
-        {
-            message: "Fruits",
-            orders: orders
-        }
-    )
-})
-
-// start the Express server
-app.listen(PORT);
+server.listen(PORT);
